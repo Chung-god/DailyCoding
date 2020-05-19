085fbb97205757243c63d1542e3d812aa17987d1
현청 박 <qkrguscjd2@naver.com>
2020-05-19 09:51:49 +0900

    Q24~37 CodeUp Basic 100

---
+++ Q26/Q26/Q26.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main() {
+	/*int h, m, s;
+	scanf_s("%d:%d:%d", &h, &m, &s);
+	printf("%d", m);
+	*/
+
+	/*
+	int year, month, day;
+	scanf_s("%d.%d.%d", &year, &month, &day);
+	printf("%02d-%02d-%04d", day, month, year);
+	*/
+	
+	/*
+	unsigned int u;
+	scanf_s("%u", &u);
+	printf("%u", u);
+	*/
+
+	/*
+	double d;
+	scanf_s("%lf", &d);
+	printf("%.11lf", d);
+	*/
+
+	/*
+	long long int n;
+	scanf_s("%lld", &n);
+	printf("%lld", n);
+	*/
+
+	/*
+	int n;
+	scanf_s("%d", &n);
+	printf("%o", n);
+	*/
+
+	/*
+	int n;
+	scanf_s("%d", &n)
+	printf("%X", n);
+	*/
+	
+	/*
+	int o;
+	scanf_s("%x", &o);
+	printf("%o", o);
+	*/
+	
+	/*
+	char c;
+	scanf_s("%c", &c);
+	printf("%d", c);
+	*/
+
+	/*
+	int d;
+	scanf_s("%d", &d);
+	printf("%c", d);
+	*/
+	}
