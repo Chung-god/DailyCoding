8c631e21f735341214f47454422227773d0028ef
현청 박 <qkrguscjd2@naver.com>
2020-05-20 16:09:55 +0900

    Q1039~Q1050

---
+++ Qustion/Qustion/Qustion.cpp
@@ -0,0 +1,104 @@
+﻿// Qustion.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    /*1039
+    long n1,n2;
+    scanf_s("%ld %ld", &n1, &n2);
+    printf("%ld", n1+n2);
+    */
+
+    /*1040
+    long long n1, n2;
+    scanf_s("%lld %lld", &n1, &n2);
+    printf("%lld", n1 + n2);
+    */
+
+    /*1041
+    int a;
+    scanf_s("%d", &a);
+    printf("%d", -a);
+    */
+    
+    /*1042
+    char c;
+    scanf_s("%c", &c);
+    printf("%c", c + 1);
+    */
+
+    /*1043
+    int a, b;
+    scanf_s("%d %d", &a, &b);
+    printf("%d", a % b);
+    */
+    
+    /*1044
+    long long a;
+    scanf_s("%lld", &a);
+    ++a;
+    printf("%lld",a);
+    */
+
+    /*1045
+    int n1, n2;
+    scanf_s("%d %d", &n1, &n2);
+    printf("%d\n", n1 + n2);
+    printf("%d\n", n1 - n2);
+    printf("%d\n", n1 * n2);
+    printf("%d\n", n1 / n2);
+    printf("%d\n", n1 % n2);
+    printf("%.2f\n", (float)n1 / n2);
+    */
+
+    /*1046
+    long long int n1, n2, n3;
+    scanf_s("%lld %lld %lld", &n1, &n2, &n3);
+    printf("%lld\n", n1 + n2 + n3);
+    printf("%.1lf", (double)(n1 + n2 + n3) / 3);
+    */
+
+    /*1047
+    int a;
+    scanf_s("%d", &a);
+    printf("%d", a<<1);
+    */
+
+    /*1048
+    int a,b;
+    scanf_s("%d %d", &a, &b);
+    printf("%d", a << b);
+    */
+
+    /*1049
+    int a, b;
+    scanf_s("%d %d", &a, &b);
+    if (a > b)
+        printf("1");
+    else
+        printf("0");
+    */
+
+    /*1050
+    int a, b;
+    scanf_s("%d %d", &a, &b);
+    if (a == b)
+        printf("1");
+    else
+        printf("0");
+    */
+
+    }
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
