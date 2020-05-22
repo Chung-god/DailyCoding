a41d3b0bca9619d8a65f0e2680349106876faa09
HyunChungPark <qkrguscjd2@naver.com>
2020-05-22 11:12:30 +0900

    Q1081~Q1085 1085 overflow

---
+++ Question_from1081/Question_from1081/Question_from1081.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main() {
+	/*1081
+	int n, m;
+	scanf_s("%d %d", &n, &m);
+	int i, j;
+	for (i = 1; i <= n; i++)
+		for (j = 1; j <= m; j++)
+			printf("%d %d\n", i, j);*/
+	/*1082
+	int n;
+	int i;
+	scanf_s("%X", &n);
+	for (i = 1; i <= 15; i++)
+		printf("%X*%X=%X\n", n, i, n * i);*/
+	
+	//1083
+	//int n;
+	//int i;
+	//scanf_s("%d", &n);
+	//for (i = 1; i <= n; i++) {
+	//	if (i % 3 == 0)
+	//		printf("X ");
+	//	else
+	//		printf("%d ", i);
+	//}
+
+
+	/*1085
+	int i, j, k;
+	int r, g, b;
+	int ans = 0;
+	scanf_s("%d%d%d", &r, &g, &b);
+	for(i = 0;i < r;i++)
+		for(j = 0; j <  g; j++)
+			for (k = 0; k < b; k++) {
+				printf("%d %d %d\n", i, j, k);
+				ans++;
+			}
+	printf("%d", ans);*/
+
+	//int h, b, c, s;
+	////1초동안 마이크로 소리 강약 체크하는 수 h
+	////한번 체크한 결과를 저장하는 비트 b
+	////좌우 등 소리를 저장할 트랙 개수인 채널 c
+	////녹음할 시간 s
+
+
+	//scanf_s("%d %d %d %d", &h, &b, &c, &s);
+
+	//double sum=1;
+	//sum = (unsigned int)(h * b * c * s) ;//산술 오버플로우가 발생하기 때문에 형을 변환해 주어야한다.
+	//printf("%.1f MB", (double)sum/ 8388608 );
+
+
+}
