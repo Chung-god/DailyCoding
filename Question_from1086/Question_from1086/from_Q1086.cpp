232cb7503cd0d1911f4454310084caec56fbcd14
HyunChungPark <qkrguscjd2@naver.com>
2020-05-23 23:56:54 +0900

    Q1086~Q1088

---
+++ Question_from1086/Question_from1086/from_Q1086.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+	/*1086 Calculate Pixel
+	int w, h, b;
+	scanf_s("%d%d%d", &w, &h, &b);
+	double sum = 1;
+	sum = (unsigned int)(w * h * b);
+	printf("%.2f MB", sum / 1024 / 1024 / 8);*/
+
+	/*1087
+	int i;
+	int num,sum = 0;
+	scanf_s("%d", &num);
+
+	for (i = 1; sum < num; i++) {
+		sum += i;
+	}
+	printf("%d", sum);*/
+
+	/*1088
+	int i;
+	int num;
+	scanf_s("%d", &num);
+	for (i = 1; i <= num; i++) {
+		if (i % 3 != 0) {
+			printf("%d ", i);
+		}
+	}*/
+}
