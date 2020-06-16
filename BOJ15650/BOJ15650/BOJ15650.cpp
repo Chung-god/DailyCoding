db1935ed64960162e156dbb38ec93cdd36a5b9eb
현청 박 <qkrguscjd2@naver.com>
2020-06-16 14:58:41 +0900

    Not work

---
+++ BOJ15650/BOJ15650/BOJ15650.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#define MAX 9
+using namespace std;
+
+int N, M;
+int arr[MAX];
+bool visited[MAX];
+
+
+void dfs(int cnt, int b) {
+    if (cnt == M) {
+        for (int i = 0; i < M; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+    else {
+        for (int i = 1; i <= N; i++) {
+            if (visited[i])
+                continue;
+            else if (i < b)
+                continue;
+            else {
+                visited[i] = true;
+                arr[cnt] = i;
+                dfs(cnt + 1, i);
+                arr[cnt] = 0;
+                visited[i] = false;
+            }
+        }
+
+    }
+    
+    return;
+}
+
+
+
+int main()
+{
+    cin >> N >> M;
+    dfs(1, 0);
+    return 0;
+}
